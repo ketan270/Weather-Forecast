94b7f898b7eae59e0d3cd3a4536462c392ae8a77
Ketan_Sharma <146920220+ketan270@users.noreply.github.com>
2024-06-19 23:29:14 +0530

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,8 +5,8 @@ const Footer = () => {
     <div className="footer">
       <h3 className="footer__text">
         Designed and Built by{" "}
-        <a href="https://github.com/victorcodess" target="_blank">
-          Victor Williams
+        <a href="https://github.com/ketan270" target="_blank">
+          Ketan Sharma
         </a>{" "}
         with React, CSS, Chart.js, GeolocationAPI, and WeatherAPI.
       </h3>
